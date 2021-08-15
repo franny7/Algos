cce01b801a128699b83a17dc3cc657d428492833
Francisco Martinez <panchetz7@gmail.com>
2021-08-15 14:46:10 -0700

    hashmap solution

--- leetcode/isAnagram.js
+++ leetcode/isAnagram.js
@@ -17,19 +17,48 @@
   @param {string} t
   @return {boolean}
  */
+
+// Regular Solution. Down below is the mapping solution.
+
+// var isAnagram = function (s, t) {
+//   if (s.length !== t.length) {
+//     return false;
+//   }
+
+//   let sortedS = s.split('').sort().join('');
+//   let sortedT = t.split('').sort().join('');
+
+//   if (sortedS === sortedT) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// Hashmap solution here!
+
 var isAnagram = function (s, t) {
-  if (s.length !== t.length) {
+  let lengthS = s.length;
+  let lengthT = t.length;
+  let map = {};
+
+  if (lengthS !== lengthT) {
     return false;
   }
 
-  let sortedS = s.split('').sort().join('');
-  let sortedT = t.split('').sort().join('');
-
-  if (sortedS === sortedT) {
-    return true;
-  } else {
-    return false;
+  for (var i = 0; i < lengthS; i++) {
+    if (!map[s[i]]) {
+      map[s[i]] = 0;
+    }
+    map[s[i]]++;
   }
+
+  for (var j = 0; j < lengthT; j++) {
+    if (!map[t[j]]) {
+      return false;
+    }
+    map[t[j]]--;
+  }
+
+  return true;
 };
-
-// Add hashmap solution here!
