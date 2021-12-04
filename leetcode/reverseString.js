a5e4896ff42b47f6efb6ce82432ec5ad55f4b805
Francisco Martinez <panchetz7@gmail.com>
2021-12-04 14:33:57 -0800

    complete w/pseudocode and most optimal solution

--- leetcode/reverseString.js
+++ leetcode/reverseString.js
@@ -1,3 +1,5 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/879/
+
 // Write a function that reverses a string. The input string is given as an array of characters s.
 
 // Example 1:
@@ -13,11 +15,16 @@
 // s[i] is a printable ascii character.
 
 var reverseString = function (s) {
+  // 1. Create two pointers, one on the first character and the other at the last character
   let left = 0;
   let right = s.length - 1;
+  // 2. loop through the string and continue while the two pointers dont intersect
   while (left < right) {
+    // 3. swap the left pointer value with the right pointer value and increment and decrement until the while loop is done iterating
     [s[left], s[right]] = [s[right], s[left]];
     left++;
     right--;
   }
 };
+
+//////// Most optimal solution!!!
