b2be7a4bbcfc932f2a8a2f0e71f2d23b9475d9ef
Francisco Martinez <panchetz7@gmail.com>
2021-12-04 15:35:54 -0800

    completed better solution w/ pseudocode

--- leetcode/moveZeroes.js
+++ leetcode/moveZeroes.js
@@ -1,3 +1,5 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/567/
+
 // Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
 // Note that you must do this in-place without making a copy of the array.
@@ -22,15 +24,40 @@
  */
 
 var moveZeroes = function (nums) {
-  let reserve = [];
+  // 1. create a temporary array to hold our zeroes
+  let zeroes = [];
+  // 2. loop through our nums array
   for (let i = 0; i < nums.length; i++) {
+    // 3. if our current element is equal to 0, throw our current element into our zeroes array and splice the index we are on, also decrement one because our current loop auto increments so it will skip an index if we don't decrement
     if (nums[i] === 0) {
-      reserve.push(nums[i]);
+      zeroes.push(nums[i]);
       nums.splice(i, 1);
       i--;
     }
   }
-  for (let j = 0; j < reserve.length; j++) {
-    nums.push(reserve[j]);
+  // 4. loop through our zeroes array and push each zero to our nums array
+  for (let j = 0; j < zeroes.length; j++) {
+    nums.push(zeroes[j]);
   }
 };
+
+// Below is an example of using two pointers, but in this case it's not as effecient as using array data structure. The above array solution is more effecient in this case.
+
+//     let left = 0;
+//     let right = nums.length - 1;
+
+//     if(!nums.includes(0)) {
+//         return nums;
+//     }
+//     while(left < right) {
+//         if(nums[left] !== 0) {
+//             left++;
+//         }
+//         if(nums[right] === 0) {
+//             right--;
+//         }
+//         if(nums[left] === 0) {
+//             nums.push(nums.splice(nums.indexOf(nums[left]), 1)[0]);
+//         }
+//     }
+//     return nums;
