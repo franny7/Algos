6a3d4700ac0a9582ccb3ffafec2a0b437c8c005b
Francisco Martinez <panchetz7@gmail.com>
2021-12-05 22:22:36 -0800

    added link for leetcode

--- leetcode/isAnagram.js
+++ leetcode/isAnagram.js
@@ -1,3 +1,5 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/882/
+
 // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 // Example 1:
