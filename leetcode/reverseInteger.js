f230240c068ea7186f5de1afbf3ad9227dd2d31e
Francisco Martinez <panchetz7@gmail.com>
2021-12-09 20:57:13 -0800

    updated pseudocode

--- leetcode/reverseInteger.js
+++ leetcode/reverseInteger.js
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/reverse-integer/
+
 // Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 // Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 
@@ -25,13 +27,18 @@
   @return {number}
  */
 var reverse = function (x) {
+  // 1. turn number into string and split them into their own index turning it into an array
   let string = x.toString().split('');
+  // 2. check if the number is negative than remove the dash
   if (string[0] === '-') {
     string.splice(0, 1);
   }
 
+  // 3. reverse our array (string) and join the indices to turn into a number again
   let number = string.reverse().join('');
+  // 4. create answer variable
   let answer;
+  // 5. check if number is greater than 32 bit, if so answer = 0. if x is a negative number, throw a dash in front of the new reversed number. else just set the answer = number.
   if (number < -2147483648 || number > 2147483648) {
     answer = 0;
   } else if (x < 0) {
@@ -40,5 +47,6 @@ var reverse = function (x) {
     answer = number;
   }
 
+  // 6. return answer
   return answer;
 };
