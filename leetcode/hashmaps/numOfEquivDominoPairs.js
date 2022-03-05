113aae679edb161e81bc0c72ed525c6d32847739
Francisco Martinez <panchetz7@gmail.com>
2022-03-04 17:58:09 -0800

    solution w/o pseudocode

---
+++ leetcode/hashmaps/numOfEquivDominoPairs.js
@@ -0,0 +1,51 @@
+// https://leetcode.com/problems/number-of-equivalent-domino-pairs/
+
+/*
+Given a list of dominoes, dominoes[i] = [a, b] is equivalent to dominoes[j] = [c, d] if and only if either (a == c and b == d), or (a == d and b == c) - that is, one domino can be rotated to be equal to another domino.
+
+Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and dominoes[i] is equivalent to dominoes[j].
+ 
+
+Example 1:
+Input: dominoes = [[1,2],[2,1],[3,4],[5,6]]
+Output: 1
+
+Example 2:
+Input: dominoes = [[1,2],[1,2],[1,1],[1,2],[2,2]]
+Output: 3
+ 
+Constraints:
+1 <= dominoes.length <= 4 * 104
+dominoes[i].length == 2
+1 <= dominoes[i][j] <= 9
+*/
+
+/*
+  @param {number[][]} dominoes
+  @return {number}
+ */
+var numEquivDominoPairs = function (dominoes) {
+  let result = 0;
+  const freq = {};
+
+  for (let domino of dominoes) {
+    domino = domino.sort();
+    let key = `${domino[0]}_${domino[1]}`;
+    if (key in freq) {
+      freq[key]++;
+    } else {
+      freq[key] = 1;
+    }
+  }
+
+  for (let key in freq) {
+    let value = freq[key];
+    if (value > 1) {
+      result += (value * (value - 1)) / 2;
+    }
+  }
+
+  return result;
+};
+
+// add pseudocode!!!
