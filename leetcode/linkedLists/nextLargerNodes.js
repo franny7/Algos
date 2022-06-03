4d14acc165f007b6853e69d2afec9ae7252cb0a1
Francisco Martinez <panchetz7@gmail.com>
2022-06-03 00:01:13 -0700

    good solution w/pseudocode

--- leetcode/linkedLists/nextLargerNodes.js
+++ leetcode/linkedLists/nextLargerNodes.js
@@ -34,26 +34,34 @@ The number of nodes in the list is n.
   @return {number[]}
  */
 var nextLargerNodes = function (head) {
+  // create stack
   const stack = [];
 
+  // create count var and set current equal to head
   let count = 0;
   let current = head;
 
+  // while our current node is not null (as long as we haven't passed the last node), increment count and set current equal to our next node
   while (current !== null) {
     count++;
     current = current.next;
   }
 
+  // create result var setting it equal to an empty array of the size of our nodes
   const result = new Array(count);
 
+  // again set current equal to head so it can re start at the beginning, and creat index var starting at 0
   current = head;
   let index = 0;
 
+  // push our current value and its index into our stack
   stack.push([current.val, index]);
 
+  // again set current equal to current.next as our way of incrementing through our nodes, set index = 1
   current = current.next;
   index = 1;
 
+  // as long as we haven't passed all our nodes, while our stack is not empty and our current nodes value is greater than stacks last elements first index (which is the value of that node), create popped var to pop the last index of our stack, and push the current nodes value to our result. push the current nodes value into our stackalong with its index, increment index, and set our current to current.next.
   while (current !== null) {
     while (stack.length > 0 && current.val > stack[stack.length - 1][0]) {
       let popped = stack.pop();
@@ -64,12 +72,14 @@ var nextLargerNodes = function (head) {
     current = current.next;
   }
 
+  // if there are any remaining elements in stack at the end, that means there is no next larger node so simply pop each one and set their place in result equal to 0
   while (stack.length > 0) {
     // pop from stack and set all indexes to 0
     let popped = stack.pop();
     result[popped[1]] = 0;
   }
 
+  // retunr result
   return result;
 };
 
