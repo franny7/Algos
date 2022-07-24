4e9aa657da183310fbf64be9ddf815193a9a28c7
Francisco Martinez <panchetz7@gmail.com>
2022-07-24 00:55:52 -0700

    good solution, need pseudocode

---
+++ leetcode/stacks/backspaceCompare.js
@@ -0,0 +1,63 @@
+// https://leetcode.com/problems/backspace-string-compare/
+
+/*
+Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+
+Example 1:
+Input: s = "ab#c", t = "ad#c"
+Output: true
+Explanation: Both s and t become "ac".
+
+Example 2:
+Input: s = "ab##", t = "c#d#"
+Output: true
+Explanation: Both s and t become "".
+
+Example 3:
+Input: s = "a#c", t = "b"
+Output: false
+Explanation: s becomes "c" while t becomes "b".
+ 
+
+Constraints:
+1 <= s.length, t.length <= 200
+s and t only contain lowercase letters and '#' characters.
+
+Follow up: Can you solve it in O(n) time and O(1) space?
+*/
+
+/*
+  @param {string} s
+  @param {string} t
+  @return {boolean}
+ */
+var backspaceCompare = function (s, t) {
+  const stackS = [];
+  const stackT = [];
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (char === '#') {
+      stackS.pop();
+    } else {
+      stackS.push(char);
+    }
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    let char = t[i];
+    if (char === '#') {
+      stackT.pop();
+    } else {
+      stackT.push(char);
+    }
+  }
+
+  let wordS = stackS.join('');
+  let wordT = stackT.join('');
+
+  return wordS === wordT;
+};
